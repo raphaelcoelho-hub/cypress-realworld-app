e7520f4ad6382ef79f6d56acf32bdee69a33e786
RaphaelCoelho <raphaelmnc@gmail.com>
2026-07-27 18:39:49 -0300

    adiciona cenário de cadastro incompleto, comenta e atualiza documentação

--- cypress-realworld-app/cypress/tests/ui/cadastro.spec.js
+++ cypress-realworld-app/cypress/tests/ui/cadastro.spec.js
@@ -1,18 +1,64 @@
 describe('Cadastro de usuário', () => {
     it('Deve cadastrar usuário com sucesso usando informações corretas', () => {
-    cy.visit('/signup')
 
+        // Acessa a tela de cadastro
+        cy.visit('/signup')
+
+        // Preenche os dados obrigatórios para criar um novo usuário
         cy.get('[data-test="signup-first-name"]').should('be.visible').type('Juliana');
         cy.get('[data-test="signup-last-name"]').should('be.visible').type('Dutra');
         cy.get('[data-test="signup-username"]').should('be.visible').type('juliana.dutra');
         cy.get('[data-test="signup-password"]').should('be.visible').type('julianadutra');
         cy.get('[data-test="signup-confirmPassword"]').should('be.visible').type('julianadutra');
-        cy.get('[data-test="signup-submit"]').should('be.visible').click();
-        cy.location('pathname').should('eq', '/signin');
         
+        // Envia o formulário de cadastro
+        cy.get('[data-test="signup-submit"]').should('be.visible').click();
+
+        // Valida o redirecionamento para a tela de login
+        cy.location('pathname').should('eq', '/signin');
+    })
+
+    it('Deve fazer login com o usuário cadastrado', () => {
+
+        // Acessa a tlea de login
+        cy.visit('/signin')
+
+        // Preenche as credenciais do usuário cadastrado
+        cy.get('[data-test="signin-username"]').should('be.visible').type('juliana.dutra');
+        cy.get('[data-test="signin-password"]').should('be.visible').type('julianadutra');
+
+        // Envia o formulário de login
+        cy.get('[data-test="signin-submit"]').click();
+
+        // Valida que o login foi realizado com sucesso
+        cy.contains('Juliana D').should('be.visible');
     })
 })
+
+
+describe('Tentar registrar um novo usuário com informações incompletas', () => {
+    it('Deve impedir o cadastro quando houver campos obrigatórios em branco', () => {
+
+        // Acessa a tela de cadastro
+        cy.visit('/signup')
+
+        // Preenche os campos obrigatórios, exceto o Last Name
+        cy.get('[data-test="signup-first-name"]').should('be.visible').type('Juliana');
         
+        // Clica no campo Last Name, digita e depois limpa para disparar validação
+        cy.get('[data-test="signup-last-name"] input').click().type('teste').clear().trigger('blur');
+        
+         
+        cy.get('[data-test="signup-username"]').should('be.visible').type('juliana.dutra');
+        cy.get('[data-test="signup-password"]').should('be.visible').type('julianadutra');
+        cy.get('[data-test="signup-confirmPassword"]').should('be.visible').type('julianadutra');
+        
+        // Valida a mensagem de erro e o estado do formulário
+        cy.get('#lastName-helper-text').should('be.visible');
+        cy.get('[data-test="signup-submit"]').should('be.disabled');
+    });
+});
+
 
 
 
--- cypress-realworld-app/cypress/tests/ui/login.spec.js
+++ cypress-realworld-app/cypress/tests/ui/login.spec.js
@@ -1,41 +1,69 @@
 describe('Login Success', () => {
- it('Deve realizar login com usuário válido', () => {
-  cy.visit('/signin');
+    it('Deve realizar login com usuário válido', () => {
+        
+      // Acessa a página de login
+        cy.visit('/signin');
 
-    cy.get('[data-test="signin-username"]').should('be.visible').type('Raphael');
-    cy.get('[data-test="signin-password"]').should('be.visible').type('05041983');
-    cy.get('[data-test="signin-submit"]').click();
-    cy.contains('Raphael A').should('be.visible');
+        // Preenche as credenciais de um usuário válido 
+        cy.get('[data-test="signin-username"]').should('be.visible').type('Raphael');
+        cy.get('[data-test="signin-password"]').should('be.visible').type('05041983');
+
+        // Envia o formulário de login
+        cy.get('[data-test="signin-submit"]').click();
+
+        // Valida que o login foi realizado com sucesso
+        cy.contains('Raphael A').should('be.visible');
     });
 });
 
 
 describe('Login com credenciais inválidas', () => {
- it('Deve retornar erro ao tentar realizar login com usuário inválido', () => {
-   cy.visit('/signin');
-    
-      cy.get('[data-test="signin-username"]').should('be.visible').type('Raphael');
-      cy.get('[data-test="signin-password"]').should('be.visible').type('s3cret');
-      cy.get('[data-test="signin-submit"]').click();
-      cy.get('[data-test="signin-error"]').should('be.visible').and('have.text', 'Username or password is invalid');
+    it('Deve retornar erro ao tentar realizar login com usuário inválido', () => {
+        // Acessa a página de login
+        cy.visit('/signin');
+
+        // Preenche um usuário inválido e uma senha válida
+        cy.get('[data-test="signin-username"]').should('be.visible').type('Raphael');
+        cy.get('[data-test="signin-password"]').should('be.visible').type('s3cret');
+
+        // Tenta realizar o login
+        cy.get('[data-test="signin-submit"]').click();
+
+        // Valida a mensagem de erro apresentada pelo sistema
+        cy.get('[data-test="signin-error"]').should('be.visible').and('have.text', 'Username or password is invalid');
     });
 
- it('Deve retornar erro ao tentar realizar login com senha inválida', () => {
-   cy.visit('/signin');
-    
-      cy.get('[data-test="signin-username"]').should('be.visible').type('Katharina_Bernier');
-      cy.get('[data-test="signin-password"]').should('be.visible').type('123456');
-      cy.get('[data-test="signin-submit"]').click();
-      cy.get('[data-test="signin-error"]').should('be.visible').and('have.text', 'Username or password is invalid');
+    it('Deve retornar erro ao tentar realizar login com senha inválida', () => {
+
+        // Acessa a página de login      
+        cy.visit('/signin');
+        
+        // Preenche um usuário válido e uma senha inválida
+        cy.get('[data-test="signin-username"]').should('be.visible').type('Katharina_Bernier');
+        cy.get('[data-test="signin-password"]').should('be.visible').type('123456');
+
+        // Tenta realizar o login
+        cy.get('[data-test="signin-submit"]').click();
+
+        // Valida a mensagem de erro apresentada pelo sistema
+        cy.get('[data-test="signin-error"]').should('be.visible').and('have.text', 'Username or password is invalid');
     });
 
- it('Deve retornar erro ao tentar realizar login com usuário e senha inválidos', () => {
-   cy.visit('/signin');
-    
-      cy.get('[data-test="signin-username"]').should('be.visible').type('Raphael');
-      cy.get('[data-test="signin-password"]').should('be.visible').type('123456');
-      cy.get('[data-test="signin-submit"]').click();
-      cy.get('[data-test="signin-error"]').should('be.visible').and('have.text', 'Username or password is invalid');
-    });
+    it('Deve retornar erro ao tentar realizar login com usuário e senha inválidos', () => {
 
+        // Acessa a página de Login
+        cy.visit('/signin');
+        
+        // Preenche credenciais inválidas
+        cy.get('[data-test="signin-username"]').should('be.visible').type('Raphael');
+        cy.get('[data-test="signin-password"]').should('be.visible').type('123456');
+
+        // Tenta realizar o login
+        cy.get('[data-test="signin-submit"]').click();
+
+        // Valida a mensagem de erro apresentada pelo sistema
+        cy.get('[data-test="signin-error"]').should('be.visible').and('have.text', 'Username or password is invalid');
+    });
 });
+
+
